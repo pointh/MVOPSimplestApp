061da9d81d0587914a0871b0ff43f6f2b89fc755
Pavel Svec <pavel.svec@itcrawl.cz>
2020-09-05 14:07:54 +0200

    Add project files.

---
+++ MVOPSimplestApp/MVOPSimplestApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVOPSimplestApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    // Forms previewer je dost k ničemu
+    // Ale XAML Reload je výborné
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // Ukončování aplikace není něco, co se od mobilního prg očekává
+            // Application.Current.Quit();
+            this.Navigation.PopAsync();
+        }
+
+        private void Button_Clicked_DalsiStrana(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new DalsiStranka());
+        }
+    }
+}
